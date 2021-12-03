9abbf5e40cfcde45927592735161f5dc0f4108eb
FXHibon <francois-xavier.hibon@iadvize.com>
2021-12-03 18:12:04 +0100

    3.1

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,40 +3,36 @@ import fs2.io.file.Path
 
 object Main extends IOApp.Simple {
 
-  override def run: IO[Unit] = fs2.io.file.Files[IO].readAll(Path("/Users/francois-xavierhibon/fxhibon/advent-of-code/src/main/resources/positions"))
+
+  override def run: IO[Unit] = fs2.io.file.Files[IO].readAll(Path("/Users/francois-xavierhibon/fxhibon/advent-of-code/src/main/resources/ex3"))
     .through(fs2.text.utf8.decode)
     .through(fs2.text.lines)
     .filter(_.nonEmpty)
-    .map(_.split(" ").toList)
-    .map {
-      case List("up", count) => Cmd.Aim(-count.toInt)
-      case List("down", count) => Cmd.Aim(+count.toInt)
-      case List("forward", count) => Cmd.Forward(count.toInt)
-    }
-    .fold(State.empty) { (state, cmd) =>
-      cmd match {
-        case Cmd.Aim(x) => state.copy(aim = state.aim + x)
-        case Cmd.Forward(x) => state.copy(horizontal = state.horizontal + x, depth = state.depth + (state.aim * x))
-      }
-    }
     .compile
-    .lastOrError
-    .flatMap(state => IO.println(state.horizontal * state.depth))
+    .toList
+    .map { lines =>
+      val columnCount = lines.head.length
 
+      val gammaRateBinary = (0 until columnCount)
+        .map { index =>
+          lines.map(_.apply(index))
+        }.map { column =>
 
-  sealed trait Cmd
+        val List(epsilon, gamma) = column
+          .groupBy(_.toString)
+          .toList
+          .sortBy(_._2.size)
+          .map(_._1)
 
-  object Cmd {
-    case class Aim(x: Int) extends Cmd
+        epsilon -> gamma
+      }
 
-    case class Forward(x: Int) extends Cmd
-  }
+      val (epsilonList, gammaList) = gammaRateBinary.unzip
+      println(epsilonList.mkString)
+      println(gammaList.mkString)
+      //      println(gammaRateBinary)
 
-  case class State(horizontal: Int, depth: Int, aim: Int)
-
-  object State {
-    def empty: State = State(horizontal = 0, depth = 0, aim = 0)
-  }
+    }
 
 
 }
